1611e104eaf351ac1a946a71f976f8440056de77
Frenzchen <franziska.gruhl@unil.ch>
2020-11-21 19:05:14 +0100

    correction of color and labeling error in dimer_age_pca.r

--- Figure_4/dimer_age_pca.r
+++ Figure_4/dimer_age_pca.r
@@ -61,7 +61,6 @@ for (i in top5) {
 	dm <- rbind(dm, data.frame(dimer=i, f=f, enr=enr))
 }
 
-
 # !!! hard-coded, files needs to be replaced with species file !!! 
 cofold <- read.table("RNAcofold_pairwise_rodents.txt", sep="\t", as.is=TRUE, header=TRUE)
 
@@ -81,37 +80,15 @@ for (i in 1:l) {
 	}
 }
 
-# sort dimer frequenvy into matrix
-mx1 <- matrix(0, ncol=l, nrow=l)
-for (i in 1:l) {
-	for (j in i:l) {
-		s <- sort(c(genes[i], genes[j]))
-		id <- paste(s[1], s[2], sep="+")
-		v1 <- dimers$V2[dimers$V1 == id & dimers$V3 == "flanking"]
-		v2 <- dimers$V2[dimers$V1 == id & dimers$V3 == "background"]
-		v <- v1/v2
-		if (length(v) == 0) {v <- 0} else {v <- v}
-		mx1[i, j] <- v
-		mx1[j, i] <- v
-	}
-}
-
-# 0.6744 -> frequency included
-# 0.6746 -> enrichment included
-# 0.7596 -> freq excluded
-
 # normalize cofold and dimer frequency matrix (mx = cofold results, df = distance matrix)
 mx <- apply(mx, 2, function(x) std01(x, min(mx), max(mx)))
 df <- apply(df, 2, function(x) std01(x, min(df), max(df)))
-mx1 <- apply(mx1, 2, function(x) std01(x, min(mx1), max(mx1)))
 
 # PCA (don't scale, because it's already standardized)
-xx <- df + mx + mx1
+xx <- df + mx
 pc <- prcomp(xx, center=TRUE, scale.=FALSE)
 d <- data.frame(pc$rotation[,1:2], samples=genes)
 d2 <- merge(d, names, by.x="samples", by.y="V1")
-
-
 
 # generate line graph
 b <- subset(dimers, V3 == "background")
@@ -172,7 +149,7 @@ if (species == "mouse") {
 	
 	# full plot
 	pdf(paste(species, "_repeatPhylogeny.pdf", sep=""), width=8.27*0.6, height=11.69/4, useDingbats=FALSE)
-		ggplot(d2) + theme_minimal(base_size=8) + geom_line(data=subset(lines, selfloop == 0), aes(x=x, y=y, colour=rev(rank)), lwd=1.5) + geom_point(data=subset(lines, selfloop == 1), aes(x=x-(abs(y)*0.02), y=y+(abs(y)*0.15), colour=rev(rank)), shape=1, size=4, stroke=2, show.legend=FALSE) + geom_point(aes(x=PC1, y=PC2, fill=V2, size=freq), shape=21) + guides(fill=guide_legend(ncol = 2, title.position="top", title="Repeat class", size=2), colour=guide_legend(ncol = 2, title.position="top", title="Top5 rank", override.aes=list(size = 1)), size=guide_legend(ncol = 2, title.position="top", title="Frequency")) + scale_colour_manual(values=c('#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5'), labels=5:1) + xlab("PC1") + ylab("PC2") + scale_fill_manual(values=c('#f768a1', '#c51b8a','#7a0177'), breaks="MIR", "SINE") + geom_text(data=unique(lines[,c(1:3)]), aes(x=x-0.025, y=y+0.05, label=repeatname), fontface="bold", size=2.8) + theme(plot.title=element_text(hjust=0, vjust=1, face="bold", size=10), legend.key.size = unit(0.5, "cm")) + ggtitle(paste("A: ", species, " repeat dimers", sep="")) 
+		ggplot(d2) + theme_minimal(base_size=8) + geom_line(data=subset(lines, selfloop == 0), aes(x=x, y=y, colour=rev(rank)), lwd=1.5) + geom_point(data=subset(lines, selfloop == 1), aes(x=x-(abs(y)*0.02), y=y+(abs(y)*0.15), colour=rev(rank)), shape=1, size=4, stroke=2, show.legend=FALSE) + geom_point(aes(x=PC1, y=PC2, fill=V2, size=freq), shape=21) + guides(fill=guide_legend(ncol = 2, title.position="top", title="Repeat class", size=2), colour=guide_legend(ncol = 2, title.position="top", title="Top5 rank", override.aes=list(size = 1)), size=guide_legend(ncol = 2, title.position="top", title="Frequency")) + scale_colour_manual(values=c('#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5'), labels=5:1) + xlab("PC1") + ylab("PC2") + scale_fill_manual(values=c('#f768a1', '#7a0177', '#feebe2')) + geom_text(data=unique(lines[,c(1:3)]), aes(x=x-0.025, y=y+0.05, label=repeatname), fontface="bold", size=2.8) + theme(plot.title=element_text(hjust=0, vjust=1, face="bold", size=10), legend.key.size = unit(0.5, "cm")) + ggtitle(paste("A: ", species, " repeat dimers", sep="")) 
 	dev.off()
 
 # rat
@@ -183,7 +160,7 @@ if (species == "mouse") {
 	
 	# full plot
 	pdf(paste(species, "_repeatPhylogeny.pdf", sep=""), width=8.27*0.6, height=11.69/4, useDingbats=FALSE)
-		ggplot(d2) + theme_minimal(base_size=8) + geom_line(data=subset(lines, selfloop == 0), aes(x=x, y=y, colour=rev(rank)), lwd=1.5) + geom_point(data=subset(lines, selfloop == 1), aes(x=x-(abs(y)*0.005), y=y+(abs(y)*0.004), colour=rev(rank)), shape=1, size=4, stroke=2, show.legend=FALSE) + geom_point(aes(x=PC1, y=PC2, fill=V2, size=freq), shape=21) + guides(fill=guide_legend(ncol = 2, title.position="top", title="Repeat class", size=2), colour=guide_legend(ncol = 2, title.position="top", title="Top5 rank", override.aes=list(size = 1)), size=guide_legend(ncol = 2, title.position="top", title="Frequency")) + scale_colour_manual(values=c('#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5'), labels=5:1) + xlab("PC1") + ylab("PC2") + scale_fill_manual(values=c('#feebe2','#fbb4b9','#f768a1','#c51b8a','#7a0177')) + geom_text(data=unique(lines[,c(1:3)]), aes(x=x+0.06, y=y, label=repeatname), fontface="bold", size=2.8) + theme(plot.title=element_text(hjust=0, vjust=1, face="bold", size=10), legend.key.size = unit(0.5, "cm")) + ggtitle(paste("B': ", species, " repeat dimers", sep="")) 
+		ggplot(d2) + theme_minimal(base_size=8) + geom_line(data=subset(lines, selfloop == 0), aes(x=x, y=y, colour=rev(rank)), lwd=1.5) + geom_point(data=subset(lines, selfloop == 1), aes(x=x-(abs(y)*0.005), y=y+(abs(y)*0.004), colour=rev(rank)), shape=1, size=4, stroke=2, show.legend=FALSE) + geom_point(aes(x=PC1, y=PC2, fill=V2, size=freq), shape=21) + guides(fill=guide_legend(ncol = 2, title.position="top", title="Repeat class", size=2), colour=guide_legend(ncol = 2, title.position="top", title="Top5 rank", override.aes=list(size = 1)), size=guide_legend(ncol = 2, title.position="top", title="Frequency")) + scale_colour_manual(values=c('#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5'), labels=5:1) + xlab("PC1") + ylab("PC2") + scale_fill_manual(values=c('#feebe2','#fbb4b9','#f768a1','#c51b8a','#7a0177')) + geom_text(data=unique(lines[,c(1:3)]), aes(x=x+0.06, y=y, label=repeatname), fontface="bold", size=2.8) + theme(plot.title=element_text(hjust=0, vjust=1, face="bold", size=10), legend.key.size = unit(0.5, "cm")) + ggtitle(paste("B: ", species, " repeat dimers", sep="")) 
 	dev.off()
 	
 # rhesus
@@ -194,7 +171,7 @@ if (species == "mouse") {
 
 	# full plot
 	pdf(paste(species, "_repeatPhylogeny.pdf", sep=""), width=8.27*0.6, height=11.69/4, useDingbats=FALSE)
-		ggplot(d2) + theme_minimal(base_size=8) + geom_line(data=subset(lines, selfloop == 0), aes(x=x, y=y, colour=rev(rank)), lwd=1.5) + geom_point(data=subset(lines, selfloop == 1), aes(x=x-(abs(y)*0.007), y=y+(abs(y)*0.004), colour=rev(rank)), shape=1, size=4, stroke=2, show.legend=FALSE) + geom_point(aes(x=PC1, y=PC2, fill=V2, size=freq), shape=21) + guides(fill=guide_legend(ncol = 2, title.position="top", title="Repeat class", size=2), colour=guide_legend(ncol = 2, title.position="top", title="Top5 rank", override.aes=list(size = 1)), size=guide_legend(ncol = 2, title.position="top", title="Frequency")) + scale_colour_manual(values=c('#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5'), labels=5:1) + xlab("PC1") + ylab("PC2") + scale_fill_manual(values=c('#feebe2','#fbb4b9','#f768a1','#c51b8a','#7a0177')) + geom_text(data=unique(lines[,c(1:3)]), aes(x=x+0.06, y=y, label=repeatname), fontface="bold", size=2.8) + theme(plot.title=element_text(hjust=0, vjust=1, face="bold", size=10), legend.key.size = unit(0.5, "cm")) + ggtitle(paste("C': ", species, " repeat dimers", sep="")) 
+		ggplot(d2) + theme_minimal(base_size=8) + geom_line(data=subset(lines, selfloop == 0), aes(x=x, y=y, colour=rev(rank)), lwd=1.5) + geom_point(data=subset(lines, selfloop == 1), aes(x=x-(abs(y)*0.007), y=y+(abs(y)*0.004), colour=rev(rank)), shape=1, size=4, stroke=2, show.legend=FALSE) + geom_point(aes(x=PC1, y=PC2, fill=V2, size=freq), shape=21) + guides(fill=guide_legend(ncol = 2, title.position="top", title="Repeat class", size=2), colour=guide_legend(ncol = 2, title.position="top", title="Top5 rank", override.aes=list(size = 1)), size=guide_legend(ncol = 2, title.position="top", title="Frequency")) + scale_colour_manual(values=c('#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5'), labels=5:1) + xlab("PC1") + ylab("PC2") + scale_fill_manual(values=c('#feebe2','#7a0177')) + geom_text(data=unique(lines[,c(1:3)]), aes(x=x+0.06, y=y, label=repeatname), fontface="bold", size=2.8) + theme(plot.title=element_text(hjust=0, vjust=1, face="bold", size=10), legend.key.size = unit(0.5, "cm")) + ggtitle(paste("C: ", species, " repeat dimers", sep="")) 
 	dev.off()
 
 # human
@@ -204,7 +181,7 @@ if (species == "mouse") {
 	dev.off()
 	
 	pdf(paste(species, "_repeatPhylogeny.pdf", sep=""), width=8.27*0.6, height=11.69/4, useDingbats=FALSE)
-		ggplot(d2) + theme_minimal(base_size=8) + geom_line(data=subset(lines, selfloop == 0), aes(x=x, y=y, colour=rev(rank)), lwd=1.5) + geom_point(data=subset(lines, selfloop == 1), aes(x=x+(abs(x)*0.007), y=y+(abs(y)*0.004), colour=rev(rank)), shape=1, size=4, stroke=2) + geom_point(aes(x=PC1, y=PC2, fill=V2, size=freq), shape=21) + guides(fill=guide_legend(ncol = 2, title.position="top", title="Repeat class", size=2), colour=guide_legend(ncol = 2, title.position="top", title="Top5 rank", override.aes=list(size = 1)), size=guide_legend(ncol = 2, title.position="top", title="Frequency")) + scale_colour_manual(values=c('#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5'), labels=5:1) + xlab("PC1") + ylab("PC2") + scale_fill_manual(values=c('#7a0177', '#c51b8a'), breaks=c("Alu")) + geom_text(data=unique(lines[,c(1:3)]), aes(x=x, y=y+0.075, label=repeatname), fontface="bold", size=2.8) + theme(plot.title=element_text(hjust=0, vjust=1, face="bold", size=10), legend.key.size=unit(0.5, "cm")) + ggtitle(paste("D': ", species, " repeat dimers", sep=""))
+		ggplot(d2) + theme_minimal(base_size=8) + geom_line(data=subset(lines, selfloop == 0), aes(x=x, y=y, colour=rev(rank)), lwd=1.5) + geom_point(data=subset(lines, selfloop == 1), aes(x=x+(abs(x)*0.007), y=y+(abs(y)*0.004), colour=rev(rank)), shape=1, size=4, stroke=2) + geom_point(aes(x=PC1, y=PC2, fill=V2, size=freq), shape=21) + guides(fill=guide_legend(ncol = 2, title.position="top", title="Repeat class", size=2), colour=guide_legend(ncol = 2, title.position="top", title="Top5 rank", override.aes=list(size = 1)), size=guide_legend(ncol = 2, title.position="top", title="Frequency")) + scale_colour_manual(values=c('#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5'), labels=5:1) + xlab("PC1") + ylab("PC2") + scale_fill_manual(values=c('#feebe2','#7a0177')) + geom_text(data=unique(lines[,c(1:3)]), aes(x=x, y=y+0.075, label=repeatname), fontface="bold", size=2.8) + theme(plot.title=element_text(hjust=0, vjust=1, face="bold", size=10), legend.key.size=unit(0.5, "cm")) + ggtitle(paste("D: ", species, " repeat dimers", sep=""))
 	dev.off()
 }
 
